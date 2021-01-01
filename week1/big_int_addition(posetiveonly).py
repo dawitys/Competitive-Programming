29cbeef56392b8efeacac16a652bcca34dd225c1
Dawit <dawityonas010@gmail.com>
2021-01-01 18:24:23 +0300

    big int operations

--- week1/big_int_addition(posetiveonly).py
+++ week1/big_int_addition(posetiveonly).py
@@ -1,24 +1,3 @@
-def big_int_multiplication(a,b):
-    a_str = str(a)
-    b_str = str(b)
-
-    final_result =0
-    pos = 0
-    for i in range(len(b_str)-1,-1,-1):
-        temp = multiply_by_single_digit(a,int(b_str[i]))
-        result = str(temp) + (pos*'0')
-        final_result += int(result)
-        pos += 1
-        print(final_result)
-    
-    return final_result    
-
-def multiply_by_single_digit(num, c):
-    result = 0
-    for i in range(c):
-        result = big_int_addition(result,num)
-    return result
-
 def big_int_addition(a,b):
     final = ""
     a_str, b_str= str(a), str(b)
@@ -41,5 +20,3 @@ def big_int_addition(a,b):
         final = str(temp % 10) + final
 
     return int(final)
-
-big_int_multiplication(123,120)
