baac76ce2e417691034c2493203b69f163ed5d4c
Dawit <dawityonas010@gmail.com>
2021-01-02 16:38:20 +0300

    repeated string

--- contest1/repeated_string.py
+++ contest1/repeated_string.py
@@ -1,15 +1,13 @@
 def repeatedString(s, n):
     count = 0
-    temp = s
+    length = len(s)
     while(len(s) < n):
-        val = len(temp)
-        if n//len(temp) == 0:
-            val = n % len(temp)
+        val = length
+        if n % len(s) != 0:
+            val = n % length
         s += s[:val]
         print(s)
     for i in s:
         if i == 'a':
             count += 1   
     return count
-
-print(repeatedString('sbc',10))
