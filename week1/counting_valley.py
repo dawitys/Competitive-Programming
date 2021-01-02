e9384442cccceefa5804af73bf6a0559173169d0
Dawit <dawityonas010@gmail.com>
2021-01-02 10:53:16 +0300

    counting vallies

---
+++ week1/counting_valley.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    # Write your code here
+    sea_level = 0
+    vallies = 0
+    for i in path:
+        if i == 'U':
+            sea_level += 1
+        elif i == 'D':
+            sea_level -= 1
+        if i == 'U' and sea_level == 0:
+            vallies +=1
+    return vallies
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
