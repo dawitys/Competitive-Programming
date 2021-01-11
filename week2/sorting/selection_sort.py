c3c10c01c4b2d413e30ec53ad4c77f72ee7c079f
Dawit <dawityonas010@gmail.com>
2021-01-11 16:29:33 +0300

    insertion sort

--- week2/sorting/selection_sort.py
+++ week2/sorting/selection_sort.py
@@ -1,11 +1,10 @@
 def selectionSort(A):
     for i in range(len(A)): 
-       
-    min_idx = i 
-    for j in range(i+1, len(A)): 
-        if A[min_idx] > A[j]: 
-            min_idx = j 
-                    
+        min_idx = i 
+        for j in range(i+1, len(A)): 
+            if A[min_idx] > A[j]: 
+                min_idx = j 
+                        
     A[i], A[min_idx] = A[min_idx], A[i] 
 
     return A
