38e6c0ed981d4171d407d7848582907a25ff92ec
Dawit Yonas <dawityonas010@gmail.com>
2021-01-11 22:36:46 +0630

    [Update] Pancake sort

---
+++ week2/pancake_sorting.py
@@ -0,0 +1,9 @@
+class Solution:
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        flips = []
+        prev_max = arr[-1]
+        for i in range(len(arr)-1,-1,-1):
+            if arr[i] < prev_max:
+                arr[: i].reverse()
+                flips
+            
