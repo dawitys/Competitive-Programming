9266fbc65870e035478990b35ff0d830880eb97f
Dawit <dawityonas010@gmail.com>
2021-01-21 12:34:02 +0300

    [Update] Week 4 Crawler

---
+++ week4/crawler_log_folder.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minOperations(self, logs):
+        """
+        :type logs: List[str]
+        :rtype: int
+        """
+        depth = 0
+        for log in logs:
+            if log == '../':
+                depth = max(0, depth - 1)
+            elif log != './':
+                depth += 1
+        return depth
