3dea14da843bf7a87200d62432def10542f552e7
Dawit <dawityonas010@gmail.com>
2021-01-21 12:45:30 +0300

    [Update] Week 2 Closest to origin

---
+++ week2/day7/rearrange_string.py
@@ -0,0 +1,12 @@
+
+class Solution(object):
+    def reorganizeString(self, S):
+        N = len(S)
+        A = []
+        for c, x in sorted((S.count(x), x) for x in set(S)):
+            if c > (N+1)/2: return ""
+            A.extend(c * x)
+        ans = [None] * N
+        ans[::2], ans[1::2] = A[N/2:], A[:N/2]
+        return "".join(ans)
+        
