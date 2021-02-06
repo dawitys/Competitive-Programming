311455bac1f4ea744c4581c9a26a631674837369
Dawit Yonas <dawityonas010@gmail.com>
2021-02-07 00:49:13 +0300

    Create next_grader_node.py

---
+++ week2/next_grader_node.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def nextLargerNodes(self, head: ListNode) -> List[int]:
+        res, stack = [], []
+        while head:
+            while stack and stack[-1][1] < head.val:
+                res[stack.pop()[0]] = head.val
+            stack.append([len(res), head.val])
+            res.append(0)
+            head = head.next
+        return res
