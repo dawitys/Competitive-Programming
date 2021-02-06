8cc51d412c5b1749341cf1ea08874f0e13fef467
Dawit Yonas <dawityonas010@gmail.com>
2021-02-06 09:16:31 +0300

    Create topKfrequentWords.py

---
+++ week4/topKfrequentWords.py
@@ -0,0 +1,6 @@
+class Solution:
+    def topKFrequent(self, words, k):
+        count = collections.Counter(words)
+        heap = [(-freq, word) for word, freq in count.items()]
+        heapq.heapify(heap)
+        return [heapq.heappop(heap)[1] for _ in xrange(k)]
