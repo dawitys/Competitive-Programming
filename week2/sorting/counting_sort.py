43e28fb08920698093013ab4bddcbcc00660c4ba
Dawit Yonas <dawityonas010@gmail.com>
2021-02-07 00:15:06 +0300

    Update counting_sort.py

---
+++ week2/sorting/counting_sort.py
@@ -0,0 +1,17 @@
+def countingSort(arr):
+    highest = max(arr) + 1
+    counter_arr = [0]*highest
+    n = len(arr)
+    new_arr = [0]*n
+    
+    for i in range(len(arr)):
+        counter_arr[arr[i]] += 1
+    for i in range(1,maxi):
+        counter_arr[i]+= counter_arr[i-1]
+    
+    i = n - 1
+    while i >= 0:
+        new_arr[counter_arr[arr[i]] - 1] = arr[i]
+        counter_arr[arr[i]] -= 1
+        i -= 1
+    return new_arr
