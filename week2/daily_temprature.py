acf58ef292118ba417b5b69ca7754c0ad9055c4b
Dawit Yonas <dawityonas010@gmail.com>
2021-02-07 00:58:04 +0300

    Create daily_temprature.py

---
+++ week2/daily_temprature.py
@@ -0,0 +1,13 @@
+#Question Link: https://leetcode.com/problems/daily-temperatures/
+
+class Solution:
+    def dailyTemperatures(self, T: List[int]) -> List[int]:
+        ans = [0] * len(T)
+        stack = []
+        for i, t in enumerate(T):
+            while stack and T[stack[-1]] < t:
+                cur = stack.pop()
+                ans[cur] = i - cur
+            stack.append(i)
+
+        return ans
