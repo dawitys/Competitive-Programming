ce5d5a6d0658b0516f8a13dbac34f2565dcbcd8b
Dawit Yonas <dawityonas010@gmail.com>
2021-02-07 01:13:27 +0300

    Update queue_using_stack.py

--- week2/day7/queue_using_stack.py
+++ week2/day7/queue_using_stack.py
@@ -1,1 +1,29 @@
-//TO-DO
+class MyQueue:
+    def __init__(self):
+        self.in_stack = []
+        self.out_stack = []
+
+    def push(self, x):
+        self.in_stack.append(x)
+
+    def pop(self):
+        self.peek()
+        return self.out_stack.pop()
+
+    def peek(self):
+        if not self.out_stack:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
+        return self.out_stack[-1]        
+
+    def empty(self):
+        return not self.in_stack and not self.out_stack
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
