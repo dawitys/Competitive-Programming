eff82adf4f5e65ecc6d0c2ca0120e79f86a26795
Dawit Yonas <dawityonas010@gmail.com>
2021-03-12 16:32:35 +0300

    Create bag_of_token.py

---
+++ Camp/day2Contest/bag_of_token.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+class Solution:
+    def bagOfTokensScore(self, tokens: List[int], P: int) -> int:
+        tokens.sort()
+        deque = collections.deque(tokens)
+        ans = bns = 0
+        while deque and (P >= deque[0] or bns):
+            while deque and P >= deque[0]:
+                P -= deque.popleft()
+                bns += 1
+            ans = max(ans, bns)
+
+            if deque and bns:
+                P += deque.pop()
+                bns -= 1
+
+        return ans
