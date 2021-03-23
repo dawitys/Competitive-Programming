bdb11020729f31fb4e8ccbd539a49fc6cfbe6c6d
Dawit Yonas <dawityonas010@gmail.com>
2021-03-23 17:51:57 +0300

    Create minPathSum.py

---
+++ Camp/dp_practice/minPathSum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        dp = dict()
+        
+        for i in range(len(grid)-1,-1,-1):
+            for j in range(len(grid[i])-1,-1,-1):
+                if(i == len(grid)-1 and j == len(grid[len(grid)-1]) - 1):
+                    dp[(i,j)] = grid[i][j]
+                elif(j == len(grid[i])):
+                    dp[(i,j)] = grid[i][j] + dp[(i+1,j)]
+                elif(j == 0):
+                    dp[(i,j)] = grid[i][j] + dp[(i,j+1)]
+                else:
+                    dp[(i,j)] = grid[i][j] + min(dp[(i+1,j)],dp[(i,j+1)])
+                    
+        
+        return dp[(0,0)]
