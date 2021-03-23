158edfe6d9ada23def35c5ef16a6d50d83aba07b
Dawit Yonas <dawityonas010@gmail.com>
2021-03-23 18:05:47 +0300

    Update minPathSum.py

--- Camp/dp_practice/minPathSum.py
+++ Camp/dp_practice/minPathSum.py
@@ -4,14 +4,14 @@ class Solution:
         
         for i in range(len(grid)-1,-1,-1):
             for j in range(len(grid[i])-1,-1,-1):
-                if(i == len(grid)-1 and j == len(grid[len(grid)-1]) - 1):
+                if(i == len(grid)-1 and j == len(grid[i]) - 1):
                     dp[(i,j)] = grid[i][j]
-                elif(j == len(grid[i])):
+                elif(j == len(grid[i])-1):
                     dp[(i,j)] = grid[i][j] + dp[(i+1,j)]
-                elif(j == 0):
+                elif(i == len(grid) - 1):
                     dp[(i,j)] = grid[i][j] + dp[(i,j+1)]
                 else:
                     dp[(i,j)] = grid[i][j] + min(dp[(i+1,j)],dp[(i,j+1)])
                     
-        
+                
         return dp[(0,0)]
