16e7f33d8db62a5678f439b91ea0a246b6e6cfc3
Dawit Yonas <dawityonas010@gmail.com>
2021-03-23 15:54:40 +0300

    Create wordDictionary.py

---
+++ Camp/wordDictionary.py
@@ -0,0 +1,30 @@
+class WordDictionary:
+
+    def __init__(self):
+        self.root = {}
+        
+    def addWord(self, word: str) -> None:
+        node = self.root
+        for c in word:
+            if c not in node: node[c] = {}
+            node = node[c]
+        node['*'] = False
+        
+    def search(self, w: str) -> bool:
+    	def dfs(node, i):
+    		if node == False: return False
+    		if i == L: return '*' in node
+    		if w[i] != '.':
+    			if w[i] not in node: return False
+    			return dfs(node[w[i]],i+1)
+    		for j in node.values():
+    			if dfs(j,i+1): return True
+    		return False
+    	node, L = self.root, len(w)
+    	return dfs(node,0)
+		
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
