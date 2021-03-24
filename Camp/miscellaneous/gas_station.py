fc43e606ce54ecb9fd57cea59c03b83de9d764ad
Dawit Yonas <dawityonas010@gmail.com>
2021-03-24 12:10:14 +0300

    Create gas_station.py

---
+++ Camp/miscellaneous/gas_station.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        """
+        :type gas: List[int]
+        :type cost: List[int]
+        :rtype: int
+        """
+        if len(gas) == 0 or len(cost) == 0 or sum(gas) < sum(cost):
+            return -1
+        position = 0
+        balance = 0 # current tank balance
+        for i in range(len(gas)):
+            balance += gas[i] - cost[i] # update balance
+            if balance < 0: # balance drops to negative, reset the start position
+                balance = 0
+                position = i+1
+        return position
