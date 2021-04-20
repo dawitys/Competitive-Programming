af612fc215b5b45bacc5ef0f038be2fa1f3a6b5e
Dawit Yonas <dawityonas010@gmail.com>
2021-04-20 08:14:48 +0300

    Update minOperationToMakeEqual.py

--- dailyQuestions/minOperationToMakeEqual.py
+++ dailyQuestions/minOperationToMakeEqual.py
@@ -1,7 +1,8 @@
 class Solution(object):
     def minOperations(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        return n*n//4
+        #arthematic sum
+        #return n*n//4
+        count = (n + 1) // 2
+        first = n - 1
+        last = n - (2 * count - 1)
+        return count * (first + last) // 2
